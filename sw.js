4cb0b381dbefa39bb0132b776f08f7568503891e
persimon-c <simone.dagondon1027@gmail.com>
2026-08-21 14:40:57 +0800

    bump offline cache to v2 for renderer fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // offline cache for lecture use, cache-first since the app is fully static
 // bump the cache version whenever shipped files change, old caches get deleted on activate
 
-const CACHE = "automata-meow-v1";
+const CACHE = "automata-meow-v2";
 const ASSETS = [
   "./",
   "./index.html",

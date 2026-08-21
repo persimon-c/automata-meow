19260ec6e02ddfeb58a8febc2e68b26bdff73106
persimon-c <simone.dagondon1027@gmail.com>
2026-08-21 16:09:23 +0800

    bump cache to v15 for PDA UI

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // offline cache for lecture use, cache-first since the app is fully static
 // bump the cache version whenever shipped files change, old caches get deleted on activate
 
-const CACHE = "automata-meow-v14";
+const CACHE = "automata-meow-v15";
 const ASSETS = [
   "./",
   "./index.html",

cef02320cae9d8bbd324a80c1fa8f57a6b07e646
persimon-c <simone.dagondon1027@gmail.com>
2026-08-21 20:29:28 +0800

    bump offline cache to v20 for render pipeline changes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // offline cache for lecture use, cache-first since the app is fully static
 // bump the cache version whenever shipped files change, old caches get deleted on activate
 
-const CACHE = "automata-meow-v19";
+const CACHE = "automata-meow-v20";
 const ASSETS = [
   "./",
   "./index.html",
